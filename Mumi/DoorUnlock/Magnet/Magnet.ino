c70f0b995277b511657bc29d23aea37658193656
imakin <makin.ugm@gmail.com>
2015-04-28 18:10:26 +0700

    magnet ok

--- Mumi/DoorUnlock/Magnet/Magnet.ino
+++ Mumi/DoorUnlock/Magnet/Magnet.ino
@@ -1,22 +1,24 @@
+#define SELENOID 11
+#define LAMPUMUMI 10
 static uint8_t totalUp=0; //-- Total time the selenoid has actived in seconds
+
 void setup()
 {
 	//-- Both output
 	DDRC = DDRC | (1<<7);//-- Leonardo LED
-	DDRB = DDRB | (1<<7);//-- Relay
+	pinMode(SELENOID, OUTPUT);
+	pinMode(LAMPUMUMI, OUTPUT);
 	//-- input Pullup
-	//~ PORTE = PORTE | (1<<6);//-- Leonardo PIN7
-	//~ PORTD = PORTD | (1<<7);//-- Leonardo PIN6
-	//~ PORTC = PORTC | (1<<6);//-- Leonardo PIN5
-	//~ PORTD = PORTD | (1<<4);//-- Leonardo PIN4
 	pinMode(7,INPUT);
 	pinMode(6,INPUT);
 	pinMode(5,INPUT);
 	pinMode(4,INPUT);
+	pinMode(3,INPUT); //--- this one for medallion
 	digitalWrite(7,HIGH);
 	digitalWrite(6,HIGH);
 	digitalWrite(5,HIGH);
 	digitalWrite(4,HIGH);
+	digitalWrite(3,HIGH);
 }
 void loop()
 {
@@ -31,15 +33,16 @@ void loop()
 		)
 	{
 		PORTC = PORTC & ~(1<<7); //-- Fire up relay to activate selenoid
-		PORTB = PORTB & ~(1<<7);
+		digitalWrite(SELENOID,LOW);
+		digitalWrite(LAMPUMUMI,HIGH);
 		delay(20000);
 		totalUp += 20;
 		PORTC = PORTC | (1<<7); //-- revert & close relay connections
-		PORTB = PORTB | (1<<7);
+		digitalWrite(11,HIGH);
 	}
 	else
 	{
 		PORTC = PORTC | (1<<7); //-- close relay connections
-		PORTB = PORTB | (1<<7);
+		digitalWrite(11,HIGH);
 	}
 }
