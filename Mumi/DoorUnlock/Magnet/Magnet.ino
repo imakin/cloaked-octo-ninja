1ce348a69b3e3e650e3b4b19ae7a6532fed63a9e
Izzulmakin <makin.ugm@gmail.com>
2015-06-19 11:19:33 +0700

    latest writing found, updating

--- Mumi/DoorUnlock/Magnet/Magnet.ino
+++ Mumi/DoorUnlock/Magnet/Magnet.ino
@@ -1,48 +1,77 @@
-#define SELENOID 11
-#define LAMPUMUMI 10
-static uint8_t totalUp=0; //-- Total time the selenoid has actived in seconds
+#define INPUTKUNCI 0
+#define INPUTMEDALI 1
+static const uint8_t LED[4]={5,6,10,9};
+static const uint8_t LNUM = 4;
+int LEDisON;
 
+int i;
+void LedOn()
+{
+    Serial.println("on");
+    LEDisON = 1;
+    int b;
+    for(b=0;b<255;b++)
+    {
+        for(i=0;i<LNUM;i++)
+        {
+            analogWrite(LED[i],b);
+        }
+        delay(10);
+    }
+    digitalWrite(LED[0],HIGH);
+    digitalWrite(LED[1],HIGH);
+    digitalWrite(LED[2],HIGH);
+    digitalWrite(LED[3],HIGH);
+}
+void LedOff()
+{
+    Serial.println("off");
+    LEDisON = 0;
+    int b;
+    for(b=255;b>=0;b--)
+    {
+        for(i=0;i<LNUM;i++)
+        {
+            analogWrite(LED[i],b);
+        }
+        delay(10);
+    }
+    digitalWrite(LED[0],LOW);
+    digitalWrite(LED[1],LOW);
+    digitalWrite(LED[2],LOW);
+    digitalWrite(LED[3],LOW);
+}
 void setup()
 {
-	//-- Both output
-	DDRC = DDRC | (1<<7);//-- Leonardo LED
-	pinMode(SELENOID, OUTPUT);
-	pinMode(LAMPUMUMI, OUTPUT);
-	//-- input Pullup
-	pinMode(7,INPUT);
-	pinMode(6,INPUT);
-	pinMode(5,INPUT);
-	pinMode(4,INPUT);
-	pinMode(3,INPUT); //--- this one for medallion
-	digitalWrite(7,HIGH);
-	digitalWrite(6,HIGH);
-	digitalWrite(5,HIGH);
-	digitalWrite(4,HIGH);
-	digitalWrite(3,HIGH);
+    pinMode(11,OUTPUT);//Relay
+    pinMode(INPUTKUNCI,INPUT);
+    pinMode(INPUTMEDALI,INPUT);
+    
+    digitalWrite(INPUTKUNCI,HIGH);
+    digitalWrite(INPUTMEDALI,HIGH);
+    
+    Serial.begin(9600);
 }
 void loop()
 {
-	if (totalUp>120)
-		while(1); //-- Lock it to avoid selenoid burn. Restart the system by repowering it
-	
-	if (
-			digitalRead(7)==LOW &&
-			digitalRead(6)==LOW &&
-			digitalRead(5)==LOW &&
-			digitalRead(4)==LOW
-		)
-	{
-		PORTC = PORTC & ~(1<<7); //-- Fire up relay to activate selenoid
-		digitalWrite(SELENOID,LOW);
-		digitalWrite(LAMPUMUMI,HIGH);
-		delay(20000);
-		totalUp += 20;
-		PORTC = PORTC | (1<<7); //-- revert & close relay connections
-		digitalWrite(11,HIGH);
-	}
-	else
-	{
-		PORTC = PORTC | (1<<7); //-- close relay connections
-		digitalWrite(11,HIGH);
-	}
+    if (digitalRead(INPUTKUNCI)==LOW)
+    {
+        digitalWrite(13,HIGH);
+        digitalWrite(11,LOW);//Relay
+    }
+    else
+    {
+        digitalWrite(13,LOW);
+        digitalWrite(11,HIGH);//Relay
+    }
+    if (digitalRead(INPUTMEDALI)==LOW)
+    {
+        if(LEDisON==0)
+            LedOn();
+    }
+    else
+    {
+        if(LEDisON==1)
+            LedOff();
+    }
 }
