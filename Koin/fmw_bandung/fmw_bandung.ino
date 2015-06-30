9c95750886d681b7bb4ddcea2a55679acba58cfe
imakin <makin.ugm@gmail.com>
2015-06-30 12:00:15 +0700

    fmw bandung deployed

--- Koin/fmw_bandung/fmw_bandung.ino
+++ Koin/fmw_bandung/fmw_bandung.ino
@@ -6,19 +6,37 @@
 
 #define expectedW (k3*2 + k2 + k1*2)
 #define TOLERANSI 15
-#define RELAYPIN A3
+#define RELAYPIN A2
 static float Weight=0;
 static float selisih = 0;
 
 Hx711 *scale;//(A0,A1);//-- param: A0-data 	A1-sck
 Outputs output;
 
+void RelayTes(Outputs *o)
+{
+	o->clearAll();
+	delay(1000);
+	o->setAll();
+}
+void RelayON(Outputs *o)
+{
+	o->clearAll();
+}
+void RelayOFF(Outputs *o)
+{
+	o->setAll();
+}
+
 void setup()
 {
 	Serial.begin(9600);
 	delay(1000);
-	while(!Serial);
+	//~ while(!Serial);
 	scale = new Hx711(A0,A1);
+	output.add(A2);
+	output.add(A3);
+	RelayTes(&output);
 }
 
 void loop()
@@ -40,17 +58,17 @@ void loop()
         if (selisih<TOLERANSI && selisih>-TOLERANSI)
         {
           Serial.println(" Succeed");
-          output->setAll();
+          RelayON(&output);
           delay(500);
         }
         else
         {
-            output.clearAll();
+			RelayOFF(&output);
         }
     }
     else
     {
-        output.clearAll();
+		RelayOFF(&output);
 	}
     delay(200);
 }
