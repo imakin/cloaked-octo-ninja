25c2339cc6578fe98e1e70a8583280d880ceb500
imakin <makin.ugm@gmail.com>
2015-10-22 15:02:35 +0700

    algo pintu ketok

--- PintuKetok/PintuKetok.ino
+++ PintuKetok/PintuKetok.ino
@@ -11,17 +11,33 @@
 	#define PIN_ON	LOW
 #endif
 
-unsigned long lasthit;
 
-#define SEQ_LENGTH 3
+#define SEQ_LENGTH 4
 int seq[SEQ_LENGTH] = {3, 1, 3, 1}; 
 /**	contoh: {3, 1, 3, 1}
 		3 ketok, 1 ketok, 3 ketok, 1 ketok, (toktoktok tok toktoktok tok)
 		ketuk pramuka: {3, 3, 7} 
 **/
 #define GAP 1000
+#define TOLERANCE 500 //-- milisecond
+unsigned long lasthit;
 unsigned long smallgap;
+unsigned long thisgap;
 int seq_pos, seq_sub;
+
+unsigned long selisih(unsigned long a, unsigned long b)
+{
+	//--selisih untuk unsigned
+	if (a>b)
+		return a-b;
+	else
+		return b-a;
+}
+
+void reset_seq()
+{
+	seq_sub = 0; seq_pos = 0; 
+}
 
 void setup() {
 	Serial.begin(9600);
@@ -33,10 +49,42 @@ void setup() {
 
 void loop () 
 {
+	while (digitalRead(PIN_INPUT, PIN_DEFAULT));
 	lasthit = millis();
-	if (digitalRead(PIN_INPUT, PIN_ON))
+	while (digitalRead(PIN_INPUT, PIN_ON));
+	thisgap = millis() - lasthit;
+
+	if (seq_pos==0 && seq_sub==0)
+		smallgap = thisgap;
+
+	if (selisih(thisgap, smallgap)<TOLERANCE)
 	{
-		while (digitalRead(PIN_INPUT, PIN_ON));
-		smallgap = 
+		//-- small gap, expecting seq_sub complete
+		if (seq_sub<seq[seq_pos])
+		{
+			//-- ok, advance to next
+			seq_sub = seq_sub+1;
+		}
+		else
+		{
+			reset_seq();
+		}
+	}
+	else
+	{
+		//-- big gap, expecting seq_pos advance
+		if (seq_sub==seq[seq_pos])
+		{
+			seq_sub = 0;
+			seq_pos = seq_pos+1;
+			if (seq_pos>=(SEQ_LENGTH-1))
+			{
+				//-- finish
+			}
+		}
+		else
+		{
+			reset_seq();
+		}
 	}
 }
